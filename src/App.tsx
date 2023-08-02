794bfd00f0c2941301fa9ec47a3e723f461a2687
Matt Sigouin <mattsigpip@gmail.com>
2023-08-02 18:09:45 -0400

    Starting Space mentor challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const backgroundHome =
+    process.env.PUBLIC_URL + "/background-home-desktop.jpg";
+  const containerStyle = {
+    backgroundImage: `url(${backgroundHome})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div style={containerStyle}>
+      <header>
+        {/* Needs to be a NavBar */}
+        00 Home 01 Destination 02 Crew 03 Technology
+        <p className="description">
+          So, you want to travel to
+          <h2 className="h2">Space</h2>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
